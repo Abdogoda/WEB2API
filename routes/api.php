703e3f5758285ec8625de1f884e495ae29f91d7c
Abdulrhman Goda <abdogoda0a@gmail.com>
2025-06-12 16:09:36 +0300

    Convert CategoryController with CRUD operations for api purposes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\V1\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
+Route::prefix('admin')->group(function () {
 
-Route::get('tests', function () {
-    return response()->json(['message' => 'Test route is working!']);
+    // Categories management
+    Route::apiResource('categories', CategoryController::class);
+
 });
