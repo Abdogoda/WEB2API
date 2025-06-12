5f5f1e87367ecd4d96a668973ac975933818d319
Abdulrhman Goda <abdogoda0a@gmail.com>
2025-06-12 16:02:10 +0300

    Add test route to API for validation of functionality

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('tests', function () {
+    return response()->json(['message' => 'Test route is working!']);
+});
