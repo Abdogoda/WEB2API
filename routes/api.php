3dc2b2d7854e9385670ce5730291a6adfcb45612
Abdulrhman Goda <abdogoda0a@gmail.com>
2025-06-12 16:12:13 +0300

    Refactor: Update API routes and controllers for product, category, and message management for user (public) routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
-use App\Http\Controllers\API\V1\Admin\CategoryController;
-use App\Http\Controllers\API\V1\Admin\MessageController;
-use App\Http\Controllers\API\V1\Admin\ProductController;
 use App\Http\Controllers\API\V1\Admin\RoleController;
 use App\Http\Controllers\API\V1\Admin\UserController;
+use App\Http\Controllers\API\V1\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\API\V1\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\API\V1\Admin\MessageController as AdminMessageController;
+use App\Http\Controllers\API\V1\User\CategoryController;
+use App\Http\Controllers\API\V1\User\MessageController;
+use App\Http\Controllers\API\V1\User\ProductController;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
+Route::apiResource('/products', ProductController::class)->only(['index', 'show']);
+Route::get('products/latest', [ProductController::class, 'latestProducts']);
+Route::get('products/featured', [ProductController::class, 'featuredProducts']);
+Route::get('/products/{product}/similler', [ProductController::class, 'simillerProducts']);
+
+Route::apiResource('/categories', CategoryController::class)->only(['index', 'show']);
+
+Route::post('/contact-us', [MessageController::class, 'store']);
 
 
 Route::prefix('admin')->group(function () {
@@ -20,17 +32,17 @@ Route::prefix('admin')->group(function () {
     Route::get('permissions', [RoleController::class, 'permissions']);
 
     // Categories management
-    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('categories', AdminCategoryController::class);
 
     // Products management
-    Route::resource('products', ProductController::class);
-    Route::get('products/{product}/similler', [ProductController::class, 'simillerProducts']);
-    Route::post('products/images/upload', [ProductController::class, 'uploadImages']);
-    Route::delete('products/images/{image}/delete', [ProductController::class, 'deleteImage']);
-    Route::put('products/images/{image}/primary', [ProductController::class, 'setPrimary']);
+    Route::resource('products', AdminProductController::class);
+    Route::get('products/{product}/similler', [AdminProductController::class, 'simillerProducts']);
+    Route::post('products/images/upload', [AdminProductController::class, 'uploadImages']);
+    Route::delete('products/images/{image}/delete', [AdminProductController::class, 'deleteImage']);
+    Route::put('products/images/{image}/primary', [AdminProductController::class, 'setPrimary']);
 
     // Messages management
-    Route::apiResource('messages', MessageController::class)->only(['index', 'show', 'destroy']);
-    Route::put('messages/{message}/mark-as-read', [MessageController::class, 'markAsRead'])->name('messages.mark-as-read');
-    Route::post('messages/mark-all-as-read', [MessageController::class, 'markAllAsRead'])->name('messages.mark-all-as-read');
+    Route::apiResource('messages', AdminMessageController::class)->only(['index', 'show', 'destroy']);
+    Route::put('messages/{message}/mark-as-read', [AdminMessageController::class, 'markAsRead'])->name('messages.mark-as-read');
+    Route::post('messages/mark-all-as-read', [AdminMessageController::class, 'markAllAsRead'])->name('messages.mark-all-as-read');
 });
