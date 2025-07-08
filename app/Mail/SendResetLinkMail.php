1280f4129de1dcc0f68452d2142000e7b2d9b9e6
Abdulrhman Goda <abdogoda0a@gmail.com>
2025-07-09 01:03:26 +0300

    Implement ShouldQueue interface for email classes to enable queued email sending

--- app/Mail/SendResetLinkMail.php
+++ app/Mail/SendResetLinkMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendResetLinkMail extends Mailable
+class SendResetLinkMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
